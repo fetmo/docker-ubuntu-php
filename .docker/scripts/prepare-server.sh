f079318585ad2c500fa3ec32063394e9dcbbe50f
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2017-03-09 23:20:31 +0100

    Fix php56

--- .docker/scripts/prepare-server.sh
+++ .docker/scripts/prepare-server.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
+cd /opt/docker/scripts/files/
 echo "ioncube"
-
-cd /tmp/
-wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.bz2
 tar xvjf ioncube_loaders_lin_x86-64.tar.bz2
-rm /tmp/ioncube_loaders_lin_x86-64.tar.bz2
+rm ioncube_loaders_lin_x86-64.tar.bz2
 mkdir -p /usr/local/ioncube
-cp /tmp/ioncube/ioncube_loader_lin_5.6.so /usr/local/ioncube
-rm -rf /tmp/ioncube
-cp /opt/docker/scripts/files/00-ioncube.ini /etc/php5/apache2/conf.d/00-ioncube.ini
-cp /opt/docker/scripts/files/00-ioncube.ini /etc/php5/cli/conf.d/00-ioncube.ini
+cp /opt/docker/scripts/files/ioncube/ioncube_loader_lin_5.6.so /usr/local/ioncube
+rm -rf /opt/docker/scripts/files/ioncube
+cp /opt/docker/scripts/files/00-ioncube.ini /etc/php/5.6/apache2/conf.d/00-ioncube.ini
+cp /opt/docker/scripts/files/00-ioncube.ini /etc/php/5.6/cli/conf.d/00-ioncube.ini
 
 echo ""
 echo "phpunit"
 
 
-wget https://phar.phpunit.de/phpunit-5.7.phar
 sudo mv phpunit-5.7.phar phpunit.phar
 chmod +x phpunit.phar
 sudo mv phpunit.phar /usr/local/bin/phpunit
--- .docker/scripts/setup-workspace.sh
+++ .docker/scripts/setup-workspace.sh
@@ -8,7 +8,7 @@ echo "add user docker to sudo-group"
 usermod -a -G sudo docker
 
 echo "copy config files"
-cp /opt/docker/scripts/files/xdebug.ini /etc/php5/mods-available/xdebug.ini
+cp /opt/docker/scripts/files/xdebug.ini /etc/php/5.6/mods-available/xdebug.ini
 cp /opt/docker/scripts/files/.bash_aliases /home/docker/.bash_aliases
 cp /opt/docker/scripts/files/servername.conf /etc/apache2/conf-available/servername.conf
 
@@ -21,3 +21,4 @@ wget -O - https://packagecloud.io/gpg.key | sudo apt-key add -
 echo "deb http://packages.blackfire.io/debian any main" | sudo tee /etc/apt/sources.list.d/blackfire.list
 sudo apt-get update
 sudo apt-get install blackfire-agent
+sudo apt-get install blackfire-php
