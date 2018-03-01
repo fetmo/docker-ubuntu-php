817cc79f279c4e98bf24975f1164c17cfc283de8
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2018-03-01 15:11:54 +0100

    change

--- .docker/scripts/setup-workspace.sh
+++ .docker/scripts/setup-workspace.sh
@@ -9,13 +9,8 @@ usermod -a -G sudo docker
 
 echo "copy config files"
 cp /opt/docker/scripts/files/xdebug.ini /etc/php/7.0/mods-available/xdebug.ini
-sudo cp /opt/docker/scripts/files/.bash_aliases /home/docker/.bash_aliases
+sudo cp /opt/docker/scripts/files/.bash_aliases /var/www/.bash_aliases
 sudo chown -Rf www-data:www-data /home/docker/.bash_aliases
-sudo cp /opt/docker/scripts/files/.bash_aliases /home/nexus/.bash_aliases
-sudo chown -Rf www-data:www-data /home/nexus/.bash_aliases
-sudo mkdir /home/www-data/
-sudo cp /opt/docker/scripts/files/.bash_aliases /home/www-data/.bash_aliases
-sudo chown -Rf www-data:www-data /home/www-data/
-cp /opt/docker/scripts/files/servername.conf /etc/apache2/conf-available/servername.conf
 
+cp /opt/docker/scripts/files/servername.conf /etc/apache2/conf-available/servername.conf
 ln -s ../conf-available/servername.conf /etc/apache2/conf-enabled/servername.conf
